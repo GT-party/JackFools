d9f400ee01ed2bb17b0d53c7abfa55626db40281
Geardung <github.passis228sherdon@mail.ru>
2023-12-25 02:35:28 +0300

    add webdriver downloader

--- configs.py
+++ configs.py
@@ -1,6 +1,14 @@
 class Config():
     
+    # JackBoxFools config
     version = 0.1
+    version_path = "./temp/jbfools.version"
+    
+    # Screen parameteres
     width = 80
     
-    version_path = "./temp/version.jbfools"
+    
+    # WebDriver config
+    webdriver_version_path = "./temp/webdriver.version"
+    webdriver_version = "114.0.5735.90"
+    webdriver_url = f"https://chromedriver.storage.googleapis.com/{webdriver_version}/chromedriver_win32.zip"
--- jackfool.py
+++ jackfool.py
@@ -6,12 +6,23 @@ def cls(): os.system('cls' if os.name=='nt' else 'clear')
 if __name__ == "__main__":
     cls()
     
+    # initial checks
     if not os.path.exists("./temp"): os.mkdir("./temp")
     if not os.path.exists(Config.version_path): open(Config.version_path, "w").write(str(Config.version))
+    if not os.path.exists(Config.webdriver_version_path): open(Config.webdriver_version_path, "w").write(str(Config.webdriver_version))
     
     
     if float(open(Config.version_path).read()) < Config.version:
-        # if installed version don't equal actual version
-        os.system("py -m pip install -r reqs.txt") #download new libs
-        
+        # if installed JBFools version don't equal actual version
+        os.system("py -m pip install -r reqs.txt") #install new libs
     
+    if open(Config.webdriver_version_path).read() != Config.webdriver_version_path or \
+        not os.path.exists("./temp/chromedriver.exe"):
+            
+        # if installed WebDriver version don't equal actual version
+        from modules.downloader import download
+        from zipfile import ZipFile
+        
+        download([Config.webdriver_url], dest_dir="./temp") # Download archive with webdriver
+        with ZipFile("./temp/" + Config.webdriver_url.split("/")[-1], "r") as zfile: zfile.extractall("./temp") # unpack archive
+        os.remove("./temp/" + Config.webdriver_url.split("/")[-1]) # Delete unpacked archive
