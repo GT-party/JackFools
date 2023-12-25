24c22ca5730e28c5e3525ae4cf493bdf8c1723c4
Geardung <github.passis228sherdon@mail.ru>
2023-12-25 20:43:40 +0300

    create json branch

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git clone https://github.com/Geardung/JackFools
-cd JackFools
-git config --global --add safe.directory %cd%
-py -m pip install --upgrade pip
-py -m pip install -r reqs.txt
-del install.bat
-%SystemRoot%\explorer.exe %cd%
--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git pull
-py jackfool.py
-pause
