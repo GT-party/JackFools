25a43c66dbf252fbee0c452f6da717f48879fdef
Twores <tworesoff@gmail.com>
2024-01-01 16:13:19 +0300

    1

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-git clone https://github.com/Geardung/JackFools
-cd JackFools
-git config --global --add safe.directory %cd%
-py -m pip install --upgrade pip
-py -m pip install -r reqs.txt
-del install.bat
-del README.md
-%SystemRoot%\explorer.exe %cd%
